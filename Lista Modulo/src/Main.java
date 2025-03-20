286234b82268f58cb0885b32267650accb857550
gabriel <gabriel.stocco06@gmail.com>
2025-03-20 15:31:27 -0300

    Relização dos exercícios da Lista sobre modulos

--- Lista Modulo/src/Main.java
+++ Lista Modulo/src/Main.java
@@ -7,19 +7,24 @@ public class Main {
 
         Metodos metodos = new Metodos();
 
-        //metodos.a("gabriel");
-        //metodos.b(5);
-        //metodos.c();
-        //metodos.d(2);
+        metodos.a("gabriel");
+        metodos.b(5);
+        metodos.c();
+        metodos.d(2);
 
-        //metodos.e(Arrays.asList("Maria", "Gabriel", "João", "Carlos"));
-        //System.out.println(metodos.f(Arrays.asList(5,6,9,12,2,5,15,23,65,79,156,56,9,51,65,49,15,56,49,15,56,591,86)));
+        metodos.e(Arrays.asList("Maria", "Gabriel", "João", "Carlos"));
+        System.out.println(metodos.f(Arrays.asList(5,6,9,12,2,5,15,23,65,79,156,56,9,51,65,49,15,56,49,15,56,591,86)));
+
         int[] numeros = {5,9,3,8,1,6,8};
-        //System.out.println(metodos.g(numeros));
+        System.out.println(metodos.g(numeros));
+
         Double[] array = {5.6, 6.8, 6.7, 2.9};
-        //System.out.println(String.format("%.2f", metodos.h(Arrays.asList(array))));
-        //System.out.println(metodos.i(10, 10));
-        //System.out.println(metodos.j("452.508.178/31"));
+
+        System.out.println(String.format("%.2f", metodos.h(Arrays.asList(array))));
+
+        System.out.println(metodos.i(10, 10));
+
+        System.out.println(metodos.j("452.508.178/31"));
     }
 
 }
